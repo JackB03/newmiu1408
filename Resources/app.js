a9d65b5981c35ab898527ebbd5fca81ab935402a
Jack Bonner <jackb03@fullsail.edu>
2014-08-15 11:39:30 -0500

    Changed outline of app

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,21 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+alert("Welcome to Jack Bonner's 'About Me App'!");
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var mainWindow = Ti.UI.createWindow({
+	backgroundColor: "#1C1C1C",
+	barColor: '#1C1C1C',
+	title: "Home",
+	titleAttributes:  {
+        color:'#fff',
+        font: {fontFamily:'Roboto', fontSize:24},
+        shadow:{color:'#5959c3', offset:{width:2,height:2}}
+   },
+	borderWidth: 1,
+	statusBarStyle: Titanium.UI.iPhone.StatusBar.LIGHT_CONTENT
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var nav = Titanium.UI.iOS.createNavigationWindow({
+	window: mainWindow,
 });
 
-win1.add(label1);
+nav.open();
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
