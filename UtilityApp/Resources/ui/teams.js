55c1c2b24313a2cf2c27e320058976a38e59a0cd
Jack Bonner <jackb03@fullsail.edu>
2014-08-27 19:19:54 -0500

    added json data to teams file

---
+++ UtilityApp/Resources/ui/teams.js
@@ -0,0 +1,25 @@
+// DATA
+var theData = {
+	"team": {
+		"information": [
+			{
+				"name": "Dallas Generals"
+			},
+			{
+				"name": "Richardson Cowboys"
+			},
+			{
+				"name": "Dallas Pirates"
+			},
+			{
+				"name": "Texas Cowgirls"
+			},
+			{
+				"name": "Dallas Reloaded"
+			},
+			{
+				"name": "Atlanta Anteaters"
+			},
+		]		
+	}
+};	
